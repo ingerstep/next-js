74ab8570247dd89945a0020632dff986418146b8
Stepan <stepan.karelin.1996@mail.ru>
2024-01-18 16:48:43 +0700

    init tic-tac-toe project

---
+++ tic-tac-toe/components/header/header.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import logoSrc from "./logo.svg";
+import { Profile } from "../profile";
+import { ArrowDownIcon } from "./arrow-down-icon";
+
+export function Header() {
+  return (
+    <header className="flex h-24 items-center px-8 bg-white shadow-lg">
+      <Image src={logoSrc} alt="logo" />
+      <div className="w-px h-8 bg-slate-200 mx-6"></div>
+      <button className="w-44 bg-teal-600 text-white rounded-lg px-5 py-2 text-2xl leading-tight hover:bg-teal-500 transition-colors">
+        Играть
+      </button>
+      <button className="ml-auto flex items-center gap-2 text-start text-teal-600">
+        <Profile />
+        <ArrowDownIcon />
+      </button>
+    </header>
+  );
+}
